25458567547b4c469ed2c09dee1378900683c3f5
Ehab-55 <ehab96482@gmail.com>
2022-12-18 14:45:00 -0800

    100-times_table

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_to_98 - print all natural number from n to 98
+ * @n: The number to start printing from
+ * Retrun: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf('
+			}
+		}
+	}
+}
